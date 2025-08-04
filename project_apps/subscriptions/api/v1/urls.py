bdee65155a941b353b7bb872f580871f96ccc472
Parvez Hossen <parvez@merchantbay.com>
2025-08-04 19:10:24 +0600

    add subscription cancel api

--- project_apps/subscriptions/api/v1/urls.py
+++ project_apps/subscriptions/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from project_apps.subscriptions.api.v1.views import SubscribeCreateAPIVIEW, SubscriptionListAPIVIEW
+from project_apps.subscriptions.api.v1.views import SubscribeCreateAPIVIEW, SubscriptionListAPIVIEW, CancelSubscriptionAPIVIEW
 
 app_name = 'v1'
 
 urlpatterns = [
     path('subscribe/', SubscribeCreateAPIVIEW.as_view(), name='subscribe'),
     path('subscriptions/', SubscriptionListAPIVIEW.as_view(), name='subscriptions'),
+    path('cancel/', CancelSubscriptionAPIVIEW.as_view(), name='cancel-subscription'),
 ]
--- project_apps/subscriptions/api/v1/views.py
+++ project_apps/subscriptions/api/v1/views.py
@@ -1,11 +1,15 @@
-from rest_framework import generics
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from project_apps.subscriptions.api.v1.serializers import SubscribeSerializer
+from project_apps.subscriptions.enums import SubscriptionStatus
 from project_apps.subscriptions.models import Subscription
 
 
-class SubscribeCreateAPIVIEW(generics.CreateAPIView):
+class SubscribeCreateAPIVIEW(CreateAPIView):
     serializer_class = SubscribeSerializer
     permission_classes = [IsAuthenticated]
 
@@ -15,10 +19,26 @@ class SubscribeCreateAPIVIEW(generics.CreateAPIView):
         )
 
 
-class SubscriptionListAPIVIEW(generics.ListAPIView):
+class SubscriptionListAPIVIEW(ListAPIView):
     serializer_class = SubscribeSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = None
 
     def get_queryset(self):
         return Subscription.objects.filter(user=self.request.user)
+
+
+class CancelSubscriptionAPIVIEW(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            subscription = Subscription.objects.filter(user=request.user, status=SubscriptionStatus.ACTIVE).latest(
+                'start_date')
+        except Subscription.DoesNotExist:
+            return Response({"detail": "No active subscription found."}, status=status.HTTP_404_NOT_FOUND)
+
+        subscription.status = SubscriptionStatus.CANCELLED
+        subscription.save()
+
+        return Response({"detail": "Subscription cancelled successfully."}, status=status.HTTP_200_OK)
