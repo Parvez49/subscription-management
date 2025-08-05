e7d157b870ab83d5c6319de7174d7d488d6d46d0
Parvez Hossen <parvez@merchantbay.com>
2025-08-05 23:01:07 +0600

    remove  unnecessary settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,40 +1,5 @@
 from .staging import *
 
-# https://django-storages.readthedocs.io/en/latest/#installation
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-INSTALLED_APPS += ['storages']
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-AWS_QUERYSTRING_AUTH = False
-# DO NOT change these unless you know what you're doing.
-_AWS_EXPIRY = 60 * 60 * 24 * 7
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': f'max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate',
-}
-AWS_S3_MAX_MEMORY_SIZE = env.int('AWS_S3_MAX_MEMORY_SIZE', default=100_000_000),  # 100MB
-AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', default=None)
-AWS_S3_CUSTOM_DOMAIN = env('AWS_S3_CUSTOM_DOMAIN', default=None)
-aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-# https://docs.djangoproject.com/en/dev/ref/settings/#storages
-STORAGES = {
-    'default': {
-        'BACKEND': 'storages.backends.s3.S3Storage',
-        'OPTIONS': {
-            'location': 'media',
-            'file_overwrite': False,
-            'querystring_auth': True,
-        },
-    },
-    'staticfiles': {
-        'BACKEND': 'storages.backends.s3.S3Storage',
-        'OPTIONS': {
-            'location': 'static',
-            'default_acl': 'public-read',
-        },
-    },
-}
-
-MEDIA_URL = f'https://{aws_s3_domain}/media/'
-STATIC_URL = f'https://{aws_s3_domain}/static/'
+# MEDIA_URL = f'https://{aws_s3_domain}/media/'
+# STATIC_URL = f'https://{aws_s3_domain}/static/'
--- config/settings/staging.py
+++ config/settings/staging.py
@@ -3,15 +3,3 @@ from .base import *
 # https://docs.djangoproject.com/en/dev/ref/settings/
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[])
-SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=True)
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = env.int('EMAIL_PORT')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', default=False)
-EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=False)
-DEFAULT_FROM_EMAIL = env('EMAIL_DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
